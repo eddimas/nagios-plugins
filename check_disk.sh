448d37963f1f3cb61b469c63c3c2dd338d0449ae
eduardoDimas <eddimas@gmail.com>
2015-04-28 23:33:26 -0500

    Add test notes
    Adding test result notes to the script

--- check_disk.sh
+++ check_disk.sh
@@ -11,18 +11,28 @@
 # COMMAND-LINE FOR SERVICE (EXAMPLE)
 # $USER1$/check_disk.sh!$USER1$!$HOSTNAME$!75!90
 #
-# Output example
+# Output example for OK, warning, critical and (the worst) warning + critical
+#
 # [eddimas@centos7 ~]$ ./check_disk.sh -l eddimas -H 192.168.1.74 -w 75 -c 90
 # OK. DISK STATS: / 14% of 33G, /dev 0% of 956M, /dev/shm 1% of 966M, /run 1% of 966M, /sys/fs/cgroup 0% of 966M, /boot 34% of 521M,
+#
+# [eddimas@centos7 ~]$ ./check_disk.sh -l eddimas -H 192.168.1.74 -w 30 -c 40
+# WARNING. DISK STATS: / 14% of 33G, /dev 0% of 956M, /dev/shm 1% of 966M, /run 1% of 966M, /sys/fs/cgroup 0% of 966M, /boot 34% of 521M,; Warning /boot has 34% of utilization or 175M of 521M,
+#
+# [eddimas@dhcppc10 ~]$ ./check_disk.sh -l eddimas -H 192.168.1.74 -w 15 -c 30
+# CRITICAL. DISK STATS: / 14% of 33G, /dev 0% of 956M, /dev/shm 1% of 966M, /run 1% of 966M, /sys/fs/cgroup 0% of 966M, /boot 34% of 521M,; Critical /boot has 34% of utilization or 175M of 521M,
+#
+# [eddimas@centos7 ~]$ ./check_disk.sh -l eddimas -H 192.168.1.74 -w 10 -c 30
+# CRITICAL. DISK STATS: / 14% of 33G, /dev 0% of 956M, /dev/shm 1% of 966M, /run 1% of 966M, /sys/fs/cgroup 0% of 966M, /boot 34% of 521M,; Warning / has 14% of utilization or 4.3G of 33G,; Critical /boot has 34% of utilization or 175M of 521M,
+#
 ##########################################################
 
-I=0
-ok=0
-warn=0
-crit=0
+j=0; ok=0
+warn=0; crit=0
 COMMAND='/bin/df -PH'
 TEMP_FILE="/tmp/df.$RANDOM.log"
 
+## Help funcion 
 help() {
 cat << END
 Usage :
@@ -41,6 +51,7 @@ Note : [VALUE] must be an integer.
 END
 }
 
+## Validating and setting the variables and the input args
 if [ $# -ne 8 ]
 then
         help;
@@ -58,29 +69,29 @@ do
         esac
 done
 
+## Sending the ssh request command and store the result into local log file
 SSH_COMMAND="`ssh -l $USERNAME $HOSTNAME -C $COMMAND`"
 echo "$SSH_COMMAND"  > $TEMP_FILE.tmp
 echo "`cat $TEMP_FILE.tmp | grep -v Used | grep -v opt`" > $TEMP_FILE
-rm $TEMP_FILE.tmp
+EQP_FS="`cat $TEMP_FILE | grep -v Used | grep -v opt | wc -l`"  # determine how many FS are in the server
 
-EQP_FS="`cat $TEMP_FILE | grep -v Used | grep -v opt | wc -l`"
 
-FILE=$TEMP_FILE         # READ $FILE USING THE FILE DESCRIPTORS
-exec 3<&0                       # SAVE CURRENT STDIN
-exec 0<"$FILE"          #   AND CHANGE IT TO READ FROM FILE.
+FILE=$TEMP_FILE                 # read $file using file descriptors
+exec 3<&0                       # save current stdin
+exec 0<"$FILE"                  # change it to read from file.
 
-  while read LINE; do           # USE $LINE VARIABLE TO PROCESS LINE
-      I=$((I+1))
-                        FULL[$I]=`echo $LINE | awk '{print $2}'`
-                        USED[$I]=`echo $LINE | awk '{print $3}'`
-                        FREE[$I]=`echo $LINE | awk '{print $4}'`
-                        FSNAME[$I]=`echo $LINE | awk '{print $6}'`
-                        PERCENT[$I]=`echo $LINE | awk '{print $5}' | sed 's/[%]//g'`
+  while read LINE; do           # use $LINE variable to process each line of file
+      j=$((j+1))
+                        FULL[$j]=`echo $LINE | awk '{print $2}'`
+                        USED[$j]=`echo $LINE | awk '{print $3}'`
+                        FREE[$j]=`echo $LINE | awk '{print $4}'`
+                        FSNAME[$j]=`echo $LINE | awk '{print $6}'`
+                        PERCENT[$j]=`echo $LINE | awk '{print $5}' | sed 's/[%]//g'`
   done
 exec 3<&0
+rm $TEMP_FILE.tmp $TEMP_FILE
 
-rm $TEMP_FILE
-
+## According with the number of FS determine if the traceholds are reached (one by one)
 for (( i=1; i<=$EQP_FS; i++ )); do
         if [ "${PERCENT[$i]}" -lt "$WARN" ]; then
                 ok=$((ok+1))
@@ -93,26 +104,28 @@ for (( i=1; i<=$EQP_FS; i++ )); do
         fi
 done
 
+## Set the data to show in the nagios service status 
 for (( i=1; i<=$EQP_FS; i++ )); do
         DATA[$i]="${FSNAME[$i]} ${PERCENT[$i]}% of ${FULL[$i]},"
 done
 
+## Just validate and adjust the nagios output   
 if [ "$ok" -eq "$EQP_FS" -a "$warn" -eq 0 -a "$crit" -eq 0 ]; then
-                echo "OK. DISK STATS: ${DATA[@]}"
+    echo "OK. DISK STATS: ${DATA[@]}"
     exit 0
   elif [ "$warn" -gt 0 -a "$crit" -eq 0 ]; then
-                echo "WARNING. DISK STATS: ${DATA[@]}; Warning ${WARN_DISKS[@]}"
+    echo "WARNING. DISK STATS: ${DATA[@]}; Warning ${WARN_DISKS[@]}"
     exit 1
   elif [ "$crit" -gt 0 ]; then
-  #Validate if the Warning array is empty if so remove the Warning leyend
-        if [ ${#WARN_DISKS[@]} -eq 0 ]; then 
-                echo "CRITICAL. DISK STATS: ${DATA[@]}; Critical ${CRIT_DISKS[@]}"
-                exit 2
-          else
-                echo "CRITICAL. DISK STATS: ${DATA[@]}; Warning ${WARN_DISKS[@]}; Critical ${CRIT_DISKS[@]}"
-                exit 2
-        fi
-  else
-        echo "Unknown"
-                exit 3
+      #Validate if the Warning array is empty if so remove the Warning leyend
+      if [ ${#WARN_DISKS[@]} -eq 0 ]; then 
+          echo "CRITICAL. DISK STATS: ${DATA[@]}; Critical ${CRIT_DISKS[@]}"
+          exit 2
+      else
+          echo "CRITICAL. DISK STATS: ${DATA[@]}; Warning ${WARN_DISKS[@]}; Critical ${CRIT_DISKS[@]}"
+          exit 2
+      fi
+else
+      echo "Unknown"
+      exit 3
 fi 
