b3b575bb98270402c9eb057d69cc0eee9a3140c3
edimas <edimas@workStation>
2018-11-23 17:28:06 -0600

    Adding filter rules to solve issue reported, some time ago

--- check_disk.sh
+++ check_disk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##########################################################
 #
 # Plugin To Check Free Disk Space
@@ -43,19 +43,19 @@
 
 j=0; ok=0
 warn=0; crit=0
-COMMAND='/bin/df -PH'
-TEMP_FILE="/tmp/df.$RANDOM.log"
+COMMAND="/bin/df -PH"
+TEMP_FILE="/var/tmp/df.$RANDOM"
 
-## Help funcion 
+## Help funcion
 help() {
-cat << END
+  cat << END
 Usage :
         check_disk.sh -l [STRING] -H [STRING] -w [VALUE] -c [VALUE]
 
         OPTION          DESCRIPTION
         ----------------------------------
         -h              Help
-        -l [STRING]      Remote user
+        -l [STRING]     Remote user
         -H [STRING]     Host name
         -w [VALUE]      Warning Threshold
         -c [VALUE]      Critical Threshold
@@ -68,79 +68,85 @@ END
 ## Validating and setting the variables and the input args
 if [ $# -ne 8 ]
 then
-        help;
-        exit 3;
+  help;
+  exit 3;
 fi
 
 while getopts "l:H:n:w:c:" OPT
 do
-        case $OPT in
-        l) USERNAME="$OPTARG" ;;
-        H) HOSTNAME="$OPTARG" ;;
-        w) WARN="$OPTARG" ;;
-        c) CRIT="$OPTARG" ;;
-        *) help ;;
-        esac
+  case $OPT in
+    l) USERNAME="$OPTARG" ;;
+    H) HOSTNAME="$OPTARG" ;;
+    w) WARN="$OPTARG" ;;
+    c) CRIT="$OPTARG" ;;
+    *) help ;;
+  esac
 done
 
-## Sending the ssh request command and store the result into local log file
-SSH_COMMAND="`ssh -l $USERNAME $HOSTNAME -C $COMMAND`"
-echo "$SSH_COMMAND"  > $TEMP_FILE.tmp
-echo "`cat $TEMP_FILE.tmp | grep -v Used | grep -v opt`" > $TEMP_FILE
-EQP_FS="`cat $TEMP_FILE | grep -v Used | grep -v opt | wc -l`"  # determine how many FS are in the server
+get_data () {
+  ## Sending the ssh request command and store the result into local log file
+  ssh -l ${USERNAME} ${HOSTNAME} -C ${COMMAND} > ${TEMP_FILE}.tmp
+  cat ${TEMP_FILE}.tmp | grep -v Used | grep -v '0 0 0'|grep -ve '- - -' > ${TEMP_FILE}
+  EQP_FS=$(cat ${TEMP_FILE} | grep -v Used |grep -v '0 0 0'|grep -ve '- - -' | wc -l)  # determine how many FS are in the server
+}
 
-
-FILE=$TEMP_FILE                 # read $file using file descriptors
-exec 3<&0                       # save current stdin
-exec 0<"$FILE"                  # change it to read from file.
+process_data (){
+  FILE=$TEMP_FILE                 # read $file using file descriptors
+  exec 3<&0                       # save current stdin
+  exec 0<"$FILE"                  # change it to read from file.
 
   while read LINE; do           # use $LINE variable to process each line of file
-      j=$((j+1))
-                        FULL[$j]=`echo $LINE | awk '{print $2}'`
-                        USED[$j]=`echo $LINE | awk '{print $3}'`
-                        FREE[$j]=`echo $LINE | awk '{print $4}'`
-                        FSNAME[$j]=`echo $LINE | awk '{print $6}'`
-                        PERCENT[$j]=`echo $LINE | awk '{print $5}' | sed 's/[%]//g'`
+    j=$((j+1))
+    FULL[$j]=`echo $LINE | awk '{print $2}'`
+    USED[$j]=`echo $LINE | awk '{print $3}'`
+    FREE[$j]=`echo $LINE | awk '{print $4}'`
+    FSNAME[$j]=`echo $LINE | awk '{print $6}'`
+    PERCENT[$j]=`echo $LINE | awk '{print $5}' | sed 's/[%]//g'`
+
   done
-exec 3<&0
-rm $TEMP_FILE.tmp $TEMP_FILE
+  exec 3<&0
+  #rm $TEMP_FILE.tmp $TEMP_FILE
+}
+
+get_data
+process_data
 
 ## According with the number of FS determine if the traceholds are reached (one by one)
 for (( i=1; i<=$EQP_FS; i++ )); do
-        if [ "${PERCENT[$i]}" -lt "${WARN}" ]; then
-                ok=$((ok+1))
-        elif [ "${PERCENT[$i]}" -eq "${WARN}" -o "${PERCENT[$i]}" -gt "${WARN}" -a "${PERCENT[$i]}" -lt "${CRIT}" ]; then
-                warn=$((warn+1))
-                WARN_DISKS[$warn]="${FSNAME[$i]} has ${PERCENT[$i]}% of utilization or ${USED[$i]} of ${FULL[$i]},"                
-        elif [ "${PERCENT[$i]}" -eq "${CRIT}" -o "${PERCENT[$i]}" -gt "${CRIT}" ]; then
-                crit=$((crit+1))
-                CRIT_DISKS[$crit]="${FSNAME[$i]} has ${PERCENT[$i]}% of utilization or ${USED[$i]} of ${FULL[$i]},"                
-        fi
+  if [ "${PERCENT[$i]}" -lt "${WARN}" ]; then
+    ok=$((ok+1))
+  elif [ "${PERCENT[$i]}" -eq "${WARN}" -o "${PERCENT[$i]}" -gt "${WARN}" -a "${PERCENT[$i]}" -lt "${CRIT}" ]; then
+    warn=$((warn+1))
+    WARN_DISKS[$warn]="${FSNAME[$i]} has ${PERCENT[$i]}% of utilization or ${USED[$i]} of ${FULL[$i]},"
+  elif [ "${PERCENT[$i]}" -eq "${CRIT}" -o "${PERCENT[$i]}" -gt "${CRIT}" ]; then
+    crit=$((crit+1))
+    CRIT_DISKS[$crit]="${FSNAME[$i]} has ${PERCENT[$i]}% of utilization or ${USED[$i]} of ${FULL[$i]},"
+  fi
 done
 
-## Set the data to show in the nagios service status 
+## Set the data to show in the nagios service status
 for (( i=1; i<=$EQP_FS; i++ )); do
-        DATA[$i]="${FSNAME[$i]} ${PERCENT[$i]}% of ${FULL[$i]},"
-        perf[$i]="${FSNAME[$i]}=${PERCENT[$i]}%;${WARN};${CRIT};0;;"
+  DATA[$i]="${FSNAME[$i]} ${PERCENT[$i]}% of ${FULL[$i]},"
+  perf[$i]="${FSNAME[$i]}=${PERCENT[$i]}%;${WARN};${CRIT};0;;"
 done
 
-## Just validate and adjust the nagios output   
+## Just validate and adjust the nagios output
 if [ "$ok" -eq "$EQP_FS" -a "$warn" -eq 0 -a "$crit" -eq 0 ]; then
-    echo "OK. DISK STATS: ${DATA[@]}"
-    exit 0
-  elif [ "$warn" -gt 0 -a "$crit" -eq 0 ]; then
-    echo "WARNING. DISK STATS: ${DATA[@]}_ Warning ${WARN_DISKS[@]}| ${perf[@]}"
-    exit 1
-  elif [ "$crit" -gt 0 ]; then
-      #Validate if the Warning array is empty if so remove the Warning leyend
-      if [ ${#WARN_DISKS[@]} -eq 0 ]; then 
-          echo "CRITICAL. DISK STATS: ${DATA[@]}_ Critical ${CRIT_DISKS[@]}| ${perf[@]}"
-          exit 2
-      else
-          echo "CRITICAL. DISK STATS: ${DATA[@]}_ Warning ${WARN_DISKS[@]}_ Critical ${CRIT_DISKS[@]}| ${perf[@]}"
-          exit 2
-      fi
+  echo "OK. DISK STATS: ${DATA[@]}"
+  exit 0
+elif [ "$warn" -gt 0 -a "$crit" -eq 0 ]; then
+  echo "WARNING. DISK STATS: ${DATA[@]}_ Warning ${WARN_DISKS[@]}| ${perf[@]}"
+  exit 1
+elif [ "$crit" -gt 0 ]; then
+  #Validate if the Warning array is empty if so remove the Warning leyend
+  if [ ${#WARN_DISKS[@]} -eq 0 ]; then
+    echo "CRITICAL. DISK STATS: ${DATA[@]}_ Critical ${CRIT_DISKS[@]}| ${perf[@]}"
+    exit 2
+  else
+    echo "CRITICAL. DISK STATS: ${DATA[@]}_ Warning ${WARN_DISKS[@]}_ Critical ${CRIT_DISKS[@]}| ${perf[@]}"
+    exit 2
+  fi
 else
-      echo "Unknown"
-      exit 3
-fi 
+  echo "Unknown"
+  exit 3
+fi
